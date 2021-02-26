9340297b51ef1db4980ae8809c29b2bb52e77263
konsul777 <konsul7770@gmail.com>
2021-02-26 22:57:08 +0300

    feat: implement 3 function min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ((array === undefined)||(array.length === 0)){
+        return 0;
+    }else {
+        let min = 0;
+        for (let i = 0; i < array.length; i++){
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((array === undefined)||(array.length === 0)){
+      return 0;
+  }else {
+      let max = 0;
+      for (let i = 0; i < array.length; i++){
+          if (array[i] > max){
+              max = array[i];
+          }
+      }
+      return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((array === undefined)||(array.length === 0)){
+        return 0;
+    }else {
+        return array.reduce((accum,currentval) => accum + currentval)/array.length;
+    }
 }
